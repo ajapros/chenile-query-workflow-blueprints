ddfa1f219762aa8895be33b659cdafc34258002b
Raja Shankar Kolluru <raja@thekollurus.com>
2025-12-11 19:29:38 +0530

    Added functionality to support multiple transition actions against the same event ordered by index

--- workflow-service/src/main/java/org/chenile/workflow/service/stmcmds/AbstractSTMTransitionAction.java
+++ workflow-service/src/main/java/org/chenile/workflow/service/stmcmds/AbstractSTMTransitionAction.java
@@ -6,6 +6,7 @@ import org.chenile.stm.State;
 import org.chenile.stm.StateEntity;
 import org.chenile.stm.action.STMTransitionAction;
 import org.chenile.stm.model.Transition;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Set;
 import java.util.TreeSet;
@@ -20,28 +21,13 @@ import java.util.TreeSet;
  * @param <StateEntityType> the sub-type of the state entity
  * @param <PayloadType> the type of payload
  */
+@SuppressWarnings("unchecked")
 public abstract class AbstractSTMTransitionAction<StateEntityType extends StateEntity,PayloadType>
-        implements STMTransitionAction<StateEntityType>
-                 {
-    private boolean initiateChain = false;
+        implements STMTransitionAction<StateEntityType> {
     private final Set<OrderedCommand> cset = new TreeSet<>();
+    @Autowired STMTransitionActionResolver stmTransitionActionResolver ;
+    private boolean initiateChain = false;
 
-     public class OrderedCommand implements Comparable<OrderedCommand> {
-         /**
-          * This class is needed to ensure that the commands are retrieved in the correct order
-          */
-         public int index;
-         public AbstractSTMTransitionAction<StateEntityType,PayloadType> action;
-         public OrderedCommand(int index,AbstractSTMTransitionAction<StateEntityType,PayloadType> action){
-             this.index = index;
-             this.action = action;
-         }
-
-         public int compareTo(OrderedCommand o) {
-             return (index - o.index );
-         }
-     }
-     @SuppressWarnings("unchecked")
      @Override
     public final void doTransition(StateEntityType stateEntity, Object transitionParam, State startState,
                              String eventId, State endState, STMInternalTransitionInvoker<?> stm, Transition transition) throws Exception {
@@ -55,15 +41,12 @@ public abstract class AbstractSTMTransitionAction<StateEntityType extends StateE
 
     public void addCommand(int index,AbstractSTMTransitionAction<StateEntityType,PayloadType> action){
         initiateChain = true;
-        if (index == 0)
-            throw new ConfigurationException(5000,"Index 0 is not allowed");
         if (cset.isEmpty()){
             cset.add(new OrderedCommand(index,this));
         }
         cset.add(new OrderedCommand(index,this));
     }
 
-    @SuppressWarnings("unchecked")
     private void initExecuteChain(StateEntityType stateEntity, Object transitionParam, State startState,
                                   String eventId, State endState, STMInternalTransitionInvoker<?> stm, Transition transition) throws Exception {
         for (OrderedCommand oc: cset){
@@ -86,5 +69,31 @@ public abstract class AbstractSTMTransitionAction<StateEntityType extends StateE
     public abstract void transitionTo(StateEntityType stateEntity,
               PayloadType transitionParam, State startState, String eventId, State endState,
               STMInternalTransitionInvoker<?> stm, Transition transition) throws Exception;
+
+     public class OrderedCommand implements Comparable<OrderedCommand> {
+         /**
+          * This class is needed to ensure that the commands are retrieved in the correct order
+          */
+         public int index;
+         public AbstractSTMTransitionAction<StateEntityType,PayloadType> action;
+         public OrderedCommand(int index,AbstractSTMTransitionAction<StateEntityType,PayloadType> action){
+             this.index = index;
+             this.action = action;
+         }
+
+         public int compareTo(OrderedCommand o) {
+             return (index - o.index );
+         }
+     }
+
+     private void registerAction(String eventId, int index){
+         AbstractSTMTransitionAction<StateEntityType,PayloadType> action =
+                 (AbstractSTMTransitionAction<StateEntityType,PayloadType>) stmTransitionActionResolver.getBean(eventId);
+         if (action == null)
+             throw new ConfigurationException(5001,"No transition action found for event id " + eventId);
+         if (index == 0)
+             throw new ConfigurationException(5002,"Index 0 is not allowed");
+         action.addCommand(index,this);
+     }
 
 }
